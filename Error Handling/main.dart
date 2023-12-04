afbb84093c7142b38c998d6cc99f2b75eff8a849
gonyango <grantononyango@gmail.com>
2023-12-04 09:38:48 +0300

    Checking on the upper limit creating a class implementing Exception

--- Error Handling/main.dart
+++ Error Handling/main.dart
@@ -5,5 +5,26 @@ import "checkValidInput.dart";
 void main() {
   print("How much do you want to withdraw?");
   String? amount = stdin.readLineSync();
-  print(checkValidInput(amount));
+
+  try {
+    pastLimit(int.parse(amount!));
+  } catch (e) {
+    if (e is AmountException) {
+      print(e.errorMessage());
+    } else {
+      print("An unexpected error occurred: $e");
+    }
+  }
+}
+
+class AmountException implements Exception {
+  String errorMessage() {
+    return "You cannot withdraw more than 70000";
+  }
+}
+
+void pastLimit(int amount) {
+  if (amount > 70000) {
+    throw new AmountException();
+  }
 }
