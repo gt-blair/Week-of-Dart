56357a26d13dd1b6cd8d36a8990c4d3bb591e936
gonyango <grantononyango@gmail.com>
2023-12-04 14:42:29 +0300

    Files to handle our functions

---
+++ Lets Call Classes/Class1/Functions/printOut.dart
@@ -0,0 +1,32 @@
+import "dart:io";
+
+import "../../Class1/classes.dart";
+
+void printOut(String name) {
+  String firstLetter = name[0].toLowerCase();
+  print("What is your name? ");
+  String name2 = stdin.readLineSync()!;
+  if (firstLetter == "s") {
+    print("What is your Class Stream");
+    String classStream = stdin.readLineSync()!;
+    print("List the subjects you are learning seperated by a space: ");
+    String subjects = stdin.readLineSync()!;
+
+    List<String> subjectsList = subjects.split(" ");
+
+    var student1 = Student(name2, classStream, subjectsList);
+    student1.displayInfo();
+  } else if (firstLetter == "t") {
+    print("What is your employee number");
+    String employee_number = stdin.readLineSync()!;
+    print("List the subjects you teach seperated by a space: ");
+    String subjects = stdin.readLineSync()!;
+
+    List<String> subjectsList = subjects.split(" ");
+
+    var teacher1 = Teacher(name2, employee_number, subjectsList);
+    teacher1.displayInfo();
+  } else {
+    print("Invalid Input");
+  }
+}
