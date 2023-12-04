12edd36158964806441d34e7a82abd3a70ea4ad0
gonyango <grantononyango@gmail.com>
2023-12-04 14:43:04 +0300

    Main function importing other functions

--- Lets Call Classes/Class1/main.dart
+++ Lets Call Classes/Class1/main.dart
@@ -1,34 +1,11 @@
 import 'dart:io';
 
 import '../Class1/classes.dart';
+import 'Functions/printOut.dart';
 
 void main() {
   print("Are you a Teacher or a Student:");
   String? teachOrStud = stdin.readLineSync();
 
-  if (teachOrStud == "Teacher") {
-    print("What is your name");
-    String name = stdin.readLineSync()!;
-    print("What is your employee number");
-    String employee_number = stdin.readLineSync()!;
-    print("List the subjects you teach seperated by a space: ");
-    String subjects = stdin.readLineSync()!;
-
-    List<String> subjectsList = subjects.split(" ");
-
-    var teacher1 = Teacher(name, employee_number, subjectsList);
-    teacher1.displayInfo();
-  } else if (teachOrStud == "Student"){
-    print("What is your name");
-    String name = stdin.readLineSync()!;
-    print("What is your Class Stream");
-    String classStream = stdin.readLineSync()!;
-    print("List the subjects you are learning seperated by a space: ");
-    String subjects = stdin.readLineSync()!;
-
-    List<String> subjectsList = subjects.split(" ");
-
-    var student1 = Student(name, classStream, subjectsList);
-    student1.displayInfo();
-  }
+  printOut(teachOrStud!);
 }
