3e93640d88bbdd246b5a1ce1b5c88e1a613b9d99
gonyango <grantononyango@gmail.com>
2023-12-05 10:03:42 +0300

    Added a return function

--- Lets Call Classes/Class1/Functions/printOut.dart
+++ Lets Call Classes/Class1/Functions/printOut.dart
@@ -3,6 +3,7 @@ import "dart:io";
 import "../../Class1/classes.dart";
 
 void printOut(String name) {
+  //Takes the whole name parameter and picks out the first alphabet in lowercase
   String firstLetter = name[0].toLowerCase();
   print("What is your name? ");
   String name2 = stdin.readLineSync()!;
@@ -28,5 +29,35 @@ void printOut(String name) {
     teacher1.displayInfo();
   } else {
     print("Invalid Input");
+  }
+}
+
+String returnOut(String name) {
+  //Takes the whole name parameter and picks out the first alphabet in lowercase
+  String firstLetter = name[0].toLowerCase();
+  print("What is your name? ");
+  String name2 = stdin.readLineSync()!;
+  if (firstLetter == "s") {
+    print("What is your Class Stream");
+    String classStream = stdin.readLineSync()!;
+    print("List the subjects you are learning seperated by a space: ");
+    String subjects = stdin.readLineSync()!;
+
+    List<String> subjectsList = subjects.split(" ");
+
+    var student1 = Student(name2, classStream, subjectsList);
+    return student1.returnInfo();
+  } else if (firstLetter == "t") {
+    print("What is your employee number");
+    String employee_number = stdin.readLineSync()!;
+    print("List the subjects you teach seperated by a space: ");
+    String subjects = stdin.readLineSync()!;
+
+    List<String> subjectsList = subjects.split(" ");
+
+    var teacher1 = Teacher(name2, employee_number, subjectsList);
+    return teacher1.returnInfo();
+  } else {
+    return "Invalid Entry";
   }
 }
