ec80fabb08d5acbe650ea3b33ae9d872eb1e5bc2
gonyango <grantononyango@gmail.com>
2023-12-06 20:28:52 +0300

    Game nicely chopped... Done!

--- Rock Paper Scissors/Functions/game.dart
+++ Rock Paper Scissors/Functions/game.dart
@@ -12,20 +12,17 @@ String game(String player1) {
   // Comparing them in terms of 1 2 3
   Map<String, int> numberOutcomes = {"rock": 0, "paper": 1, "scissors": 2};
 
-  if (numberOutcomes[computer.toLowerCase()]! ==
-      numberOutcomes[player1.toLowerCase()]!) {
-    return "Its a Tie, the Computer chose $computer, You chose $player1";
-  } else if ((numberOutcomes[computer.toLowerCase()]! >
-          numberOutcomes[player1.toLowerCase()]!) &
-      (numberOutcomes[computer.toLowerCase()]! -
-              numberOutcomes[player1.toLowerCase()]! ==
-          1)) {
-    return "Game Lost the Computer chose $computer, You chose $player1";
-  } else if (numberOutcomes[computer.toLowerCase()]! -
-          numberOutcomes[player1.toLowerCase()]! >
-      1) {
-    return "Game Lost the Computer chose $computer, You chose $player1";
-  } else {
+  String compLowerCase = computer.toLowerCase();
+  String play1LowerCase = player1.toLowerCase();
+
+  int compVal = numberOutcomes[compLowerCase]!;
+  int playVal = numberOutcomes[play1LowerCase]!;
+
+  if ((compVal - playVal == -1) | (compVal - playVal == 2)) {
     return "You won You chose $player1, Computer chose $computer";
+  } else if ((compVal - playVal == 1) | (compVal - playVal == -2)) {
+    return "You Lost Computer chose $computer, You chose $player1";
+  } else {
+    return "Its A Draw Computer chose $computer, You chose $player1";
   }
 }
--- Rock Paper Scissors/README.md
+++ Rock Paper Scissors/README.md
@@ -3,3 +3,10 @@
 #### Rock
 #### Paper
 #### You guessed it right Scissors
+
+#### Had a challenge at the beginning the main chunk of it was
+#### Wordy code blocks... could be evident from a past commit but tip
+#### IN A BLOCK USE THE MOST SIMPLIFIED VERSION OF A VARIABLE
+
+#### With that minor formality... let's play
+#### :game_die: :game_die:
