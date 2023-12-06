8ed97dfbca2ee5e05262354cafd44888f40c4b46
gonyango <grantononyango@gmail.com>
2023-12-06 19:44:36 +0300

    Theres still something in the game logic

--- Rock Paper Scissors/Functions/game.dart
+++ Rock Paper Scissors/Functions/game.dart
@@ -2,17 +2,28 @@ import 'dart:math';
 
 String game(String player1) {
   String computer;
-  Map<int, String> outcomes = {0: "Rock", 1: "Paper", 2: "Scissors",};
+  Map<int, String> outcomes = {
+    0: "Rock",
+    1: "Paper",
+    2: "Scissors",
+  };
   computer = outcomes[Random().nextInt(outcomes.length)]!;
 
   // Comparing them in terms of 1 2 3
   Map<String, int> numberOutcomes = {"rock": 0, "paper": 1, "scissors": 2};
 
-  if ((numberOutcomes[computer.toLowerCase()]! >
+  if (numberOutcomes[computer.toLowerCase()]! ==
+      numberOutcomes[player1.toLowerCase()]!) {
+    return "Its a Tie, the Computer chose $computer, You chose $player1";
+  } else if ((numberOutcomes[computer.toLowerCase()]! >
           numberOutcomes[player1.toLowerCase()]!) &
       (numberOutcomes[computer.toLowerCase()]! -
               numberOutcomes[player1.toLowerCase()]! ==
           1)) {
+    return "Game Lost the Computer chose $computer, You chose $player1";
+  } else if (numberOutcomes[computer.toLowerCase()]! -
+          numberOutcomes[player1.toLowerCase()]! >
+      1) {
     return "Game Lost the Computer chose $computer, You chose $player1";
   } else {
     return "You won You chose $player1, Computer chose $computer";
