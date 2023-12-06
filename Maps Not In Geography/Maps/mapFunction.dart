40dfbcdba60046fb764f0638e673c74341d9b271
gonyango <grantononyango@gmail.com>
2023-12-06 09:56:19 +0300

    What about a squareroot function

--- Maps Not In Geography/Maps/mapFunction.dart
+++ Maps Not In Geography/Maps/mapFunction.dart
@@ -5,3 +5,18 @@ List<int> squaringNumbers(List<int> numberList) {
       numberList.map((int element) => element * element).toList();
   return squaredNumbers;
 }
+
+List<int> squarerooting(List<int> numberList) {
+  List<int> squareRooted = numberList.map((int element) {
+    int number = 0;
+    // A while loop that will increament the value of number 
+    // On the last increment before turning in false in condition
+    // We get the square root
+    // Same approach can be used in finding root 3
+    while (number * number != element) {
+      number = number + 1;
+    }
+    return number;
+  }).toList();
+  return squareRooted;
+}
