14cb13f7c6eff7250b7c84167a52ab096256793c
gonyango <grantononyango@gmail.com>
2023-12-07 10:33:09 +0300

    Working on the top diagonals

--- Queens Of London/Fuctions/isSafe.dart
+++ Queens Of London/Fuctions/isSafe.dart
@@ -34,4 +34,22 @@ bool isSafe(List<List<int>> board, int col, int row) {
       return false;
     }
   }
+
+  // Check for queens to the top-left diagonal
+  // Column decreases by 1
+  // Row increases by 1
+  for (int i = row, j = col; i <= 7 && j >= 0; i++, j--) {
+    if (board[col][row] == 1) {
+      return false;
+    }
+  }
+
+  // Check for queens to the top-right diagonal
+  // Column inrease by 1
+  // Row increases by 1
+  for (int i = row, j = col; i <= 7 && j <= 7; i++, j++) {
+    if (board[col][row] == 1) {
+      return false;
+    }
+  }
 }
