d53b341a991d8b170a550d423fcee41459a9771a
gonyango <grantononyango@gmail.com>
2023-12-07 10:26:46 +0300

    Check for queens to the top or bottom

--- Queens Of London/Fuctions/isSafe.dart
+++ Queens Of London/Fuctions/isSafe.dart
@@ -1,5 +1,4 @@
 bool isSafe(List<List<int>> board, int col, int row) {
-
   // Check for queens to the left
   // Row is constant
   // Value of column changes
@@ -14,6 +13,24 @@ bool isSafe(List<List<int>> board, int col, int row) {
   // Value of column changes
   for (int i = 7; i > col; i--) {
     if (board[i][row] == 1) {
+      return false;
+    }
+  }
+
+  // Check for queens to the bottom
+  // Column is Constant
+  // Row changes
+  for (int i = 0; i < row; i++) {
+    if (board[col][i] == 1) {
+      return false;
+    }
+  }
+
+  // Check for queens to the top
+  // Column is Constant
+  // Row changes
+  for (int i = 7; i > row; i--) {
+    if (board[col][i] == 1) {
       return false;
     }
   }
