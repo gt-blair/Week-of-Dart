752005738f63fd4ec579c70034ec8feda024b9ce
gonyango <grantononyango@gmail.com>
2023-12-07 12:10:26 +0300

    A lille burn out but we be back

---
+++ Queens Of London/Functions/placeQueens.dart
@@ -0,0 +1,9 @@
+bool placeQueens(List<List<int>> board, int row) {
+  if (row == 8) {
+    // Checks when all queens are placed correctly
+    return true;
+  }
+
+  for (int col = 0; col <= 7; col++) {}
+  return false;
+}
