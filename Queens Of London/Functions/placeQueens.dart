c738ebf932f7875d0c4e341a039774710f0be39c
gonyango <grantononyango@gmail.com>
2023-12-08 09:14:38 +0300

    Initial function

--- Queens Of London/Functions/placeQueens.dart
+++ Queens Of London/Functions/placeQueens.dart
@@ -1,6 +1,6 @@
 import 'isSafe.dart';
 
-/*bool placeQueens(List<List<int>> board, int row) {
+bool placeQueens(List<List<int>> board, int row) {
   if (row == 8) {
     // Checks when all queens are placed correctly
     return true;
@@ -19,31 +19,5 @@ import 'isSafe.dart';
       board[col][row] = 0;
     }
   }
-  return false;
-}*/
-
-bool placeQueens(List<List<int>> board, int row) {
-  if (row == 8) {
-    // All queens are placed successfully
-    return true;
-  }
-
-  for (int col = 0; col < 8; col++) {
-    if (isSafe(board, row, col)) {
-      // Place queen if it's safe
-      board[row][col] = 1;
-
-      // Recur to place queens in the next row
-      if (placeQueens(board, row + 1)) {
-        return true;
-      }
-
-      // If placing the queen in the current position doesn't lead to a solution,
-      // backtrack and try the next column
-      board[row][col] = 0;
-    }
-  }
-
-  // If no column is suitable in this row, backtrack to the previous row
   return false;
 }
